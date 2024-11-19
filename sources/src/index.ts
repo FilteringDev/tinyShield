5e5c9030b2b00921c713c446321f9bbcd5cdbb1c
piquark6046 <j.w.so@adguard.com>
2024-11-19 04:43:09 +0000

    fix: Function.prototype.apply does not work

--- sources/src/index.ts
+++ sources/src/index.ts
@@ -4,10 +4,10 @@ declare const unsafeWindow: unsafeWindow
 
 const Win = typeof unsafeWindow !== 'undefined' ? unsafeWindow : window
 
-const OriginalFunctionToString = Win.Function.prototype.toString
+const OriginalArrayToString = Win.Array.prototype.toString
 const OriginalStringIncludes = Win.String.prototype.includes
 
-const ProtectedFunctionStrings = ['toString', 'apply']
+const ProtectedFunctionStrings = ['toString', 'get']
 
 Win.Function.prototype.toString = new Proxy(Win.Function.prototype.toString, {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
@@ -20,17 +20,15 @@ Win.Function.prototype.toString = new Proxy(Win.Function.prototype.toString, {
   }
 })
 
-Win.Function.prototype.apply = new Proxy(Win.Function.prototype.apply, {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-  apply(Target: typeof Function.prototype.apply, ThisArg: Function, Args: unknown[]) {
-    let FunctionText = OriginalFunctionToString.call(ThisArg) as string
-    for (const Item of [',inventoryId:', ':if("#adshield"===', ':_.ADS_FRAME,', '[new ad(this,']) {
-      if (OriginalStringIncludes.call(FunctionText, Item) as boolean) {
-        console.debug('[tinyShield]:', FunctionText, Args)
+Win.Map.prototype.get = new Proxy(Win.Map.prototype.get, {
+  apply(Target: (key: unknown) => unknown, ThisArg: Map<unknown, unknown>, Args: [unknown]) {
+    let ArgText = OriginalArrayToString.call(Args) as string
+    for (const Item of ['{"inventoryId":']) {
+      if (OriginalStringIncludes.call(ArgText, Item)) {
         throw new Error()
       }
     }
-    
+
     return Reflect.apply(Target, ThisArg, Args)
   }
 })
