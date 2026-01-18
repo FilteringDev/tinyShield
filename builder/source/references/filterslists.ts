c0bf75c392c536b7115f06e7885d1ddfdf6e09ac
piquark6046 <j.w.so@adguard.com>
2026-01-18 21:21:23 +0000

    feat: add `FetchAdShieldDomains` func to refer AG Base and uBO filters list

---
+++ builder/source/references/filterslists.ts
@@ -0,0 +1,13 @@
+import { HTTPSRequest } from '@typescriptprime/securereq'
+import { IndexAdShieldDomainsFromAG } from './filterslists/ADG.js'
+import { IndexAdShieldDomainsFromUBO } from './filterslists/uBO.js'
+
+export async function FetchAdShieldDomains(): Promise<Set<string>> {
+  const [AGDomains, UBODomains] = await Promise.all([
+    IndexAdShieldDomainsFromAG(),
+    IndexAdShieldDomainsFromUBO()
+  ])
+
+  const CombinedDomains = new Set<string>([...AGDomains, ...UBODomains])
+  return CombinedDomains
+}
