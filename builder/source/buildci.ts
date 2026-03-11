a25e92c96e7d8977a0ff08b067caf63ea221cbdf
piquark6046 <j.w.so@adguard.com>
2026-03-11 11:10:37 +0000

    chore: update dependencies

--- builder/source/buildci.ts
+++ builder/source/buildci.ts
@@ -1,9 +1,9 @@
 import * as Zod from 'zod'
 import * as Process from 'node:process'
-import { PreProcessing, PostProcessing } from '@typescriptprime/parsing'
+import { ParseArgumentsAndOptions, FilterArgumentsForOptions } from '@typescriptprime/parsing'
 import { Build, BuildOptions } from './build.js'
 
-let ParsedArgv = (await PostProcessing<BuildOptions>(PreProcessing(Process.argv))).Options
+let ParsedArgv = (await ParseArgumentsAndOptions<BuildOptions>(FilterArgumentsForOptions(Process.argv))).Options
 let Options = await Zod.strictObject({
   Minify: Zod.string().pipe(Zod.enum(['true', 'false'])).transform(Value => Value === 'true').default(true),
   UseCache: Zod.string().pipe(Zod.enum(['true', 'false'])).transform(Value => Value === 'true').default(true),
