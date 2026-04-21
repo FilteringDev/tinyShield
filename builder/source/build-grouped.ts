aeaf00e5921cbbf4212cf9dace589df8b2a05aa2
piquark6046 <j.w.so@adguard.com>
2026-04-21 01:07:58 +0000

    feat: implement `MakeGroupMetadata` method for generating domain and file metadata

--- builder/source/build-grouped.ts
+++ builder/source/build-grouped.ts
@@ -1,6 +1,8 @@
 import * as Piscina from 'piscina'
 import * as Path from 'node:path'
 import * as Process from 'node:process'
+import * as Crypto from 'node:crypto'
+import * as Fs from 'node:fs'
 import PackageJson from '@npmcli/package-json'
 import { Build, type BuildOptions } from './build-core.js'
 
@@ -25,6 +27,37 @@ export class GroupedBuild extends Build {
       Result.add(FileNameSet)
     }
     return Result
+  }
+
+  async MakeGroupMetadata() {
+    const MetadataObj: {
+      DomainMapping: Record<string, string>,
+      FileMapping: Record<string, { SHA386: string, FileName: string }>
+    } = {
+      DomainMapping: {},
+      FileMapping: {}
+    }
+
+    const FileNameList: Set<Set<string>> = this.TransformDomainSetToFileName(new Set<Set<string>>(this.FetchedDomains.get('EachDomain')))
+    const MatchingDomains: Set<Set<string>> = new Set<Set<string>>(this.FetchedDomains.get('EachDomainFull'))
+
+    for (let I = 0; I < FileNameList.size; I++) {
+      const FileName = [...[...FileNameList][I]][0]
+      const Domains = [...MatchingDomains][I]
+      const FilePath = `${this.ProjectRoot}/dist/grouped/${FileName[0]}/tinyShield-${FileName}.user.js`
+      const FileContent = await Fs.promises.readFile(FilePath)
+      const SHA386Hash = Crypto.createHash('sha384').update(FileContent).digest('hex').toUpperCase()
+
+      for (const Domain of Domains) {
+        MetadataObj.DomainMapping[Domain] = FileName
+        MetadataObj.FileMapping[FileName] = {
+          SHA386: SHA386Hash,
+          FileName: `tinyShield-${FileName}.user.js`
+        }
+      }
+    }
+    
+    await Fs.promises.writeFile(`${this.ProjectRoot}/dist/grouped/metadata.json`, JSON.stringify(MetadataObj, null, 2), 'utf-8')
   }
   
   async Build() {
